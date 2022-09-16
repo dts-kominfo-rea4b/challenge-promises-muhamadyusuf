2e9873e11c62ca76c1e05972d61be1115d0b963e
Muhamad Yusuf <muhamadyusuf@Muhamads-MacBook-Pro.local>
2022-09-16 09:21:17 +0700

    Fungsi promiseOutput sesuai ketentuan readme

--- promise.js
+++ promise.js
@@ -1,7 +1,20 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  theaderIXX = await promiseTheaterIXX();
+  theaderVGC = await promiseTheaterVGC();
+  let count = 0;
+  for (let i = 0; i < theaderIXX.length; i++) {
+    if (theaderIXX[i].hasil === emosi)
+      count++;
+  }
+  for (let i = 0; i < theaderVGC.length; i++) {
+    if (theaderVGC[i].hasil === emosi)
+      count++;
+  }
+  return count;
+};
 
 module.exports = {
   promiseOutput,
